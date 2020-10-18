3c5e0ee384f646e193941d2fb98aa1b064443295
Ajay Balkaran <ajaybalkaran@gmail.com>
2020-10-18 19:50:53 -0400

    Added jQuery code to check if a button has been pressed * If button has been pressed: 1) store id in function. 2) add button id to userClickedPattern array.

--- game.js
+++ game.js
@@ -3,6 +3,8 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
 
+var userClickedPattern = [];
+
 function nextSequence() {
 
   var randomNumber = Math.floor(Math.random() * 4);
@@ -13,4 +15,12 @@ function nextSequence() {
   $(`#${randomChosenColour}`).fadeIn(100).fadeOut(100).fadeIn(100);
   var audio = new Audio(`sounds/${randomChosenColour}.mp3`);
   audio.play();
+
+  // Check Which button is pressed
+
+  $(".btn").click(function() {
+      let userChosenColor = this.id;
+      userClickedPattern.push(userChosenColor);
+      console.log(userClickedPattern);
+  });
 }

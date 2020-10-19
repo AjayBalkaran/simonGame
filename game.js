7a7e1ae5331c5c5cf101c7f6361d81469b3ea5b9
Ajay Balkaran <ajaybalkaran@gmail.com>
2020-10-18 20:53:04 -0400

    Refactored the play Sound code to its own function

--- game.js
+++ game.js
@@ -5,6 +5,12 @@ var gamePattern = [];
 
 var userClickedPattern = [];
 
+// add Sounds to button clicks
+function playSound(name) {
+    var audio = new Audio(`sounds/${name}.mp3`);
+    audio.play();
+}
+
 function nextSequence() {
 
   var randomNumber = Math.floor(Math.random() * 4);
@@ -13,14 +19,14 @@ function nextSequence() {
 
   // flash button and play sound
   $(`#${randomChosenColour}`).fadeIn(100).fadeOut(100).fadeIn(100);
-  var audio = new Audio(`sounds/${randomChosenColour}.mp3`);
-  audio.play();
-
+  playSound(randomChosenColour);
+  
   // Check Which button is pressed
 
   $(".btn").click(function() {
       let userChosenColor = this.id;
       userClickedPattern.push(userChosenColor);
       console.log(userClickedPattern);
+      playSound(userChosenColor);
   });
 }

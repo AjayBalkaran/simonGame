0b27e79546f187828f7d5ab34c4afd6e3d5b8fb3
Ajay Balkaran <ajaybalkaran@gmail.com>
2020-10-19 20:35:51 -0400

    Updated check answer function. Now Works :)

--- game.js
+++ game.js
@@ -16,23 +16,28 @@ function playSound(name) {
     audio.play();
 }
 
-function checkAnswer(currentLevel) {
-  // for (i = 0; i < currentLevel; i++) {
-  //   if (userClickedPattern === gamePattern) {
-  //     console.log("success");
-  //   } else {
-  //     console.log("wrong");
-  //   }
-  // }
 
-  if (userClickedPattern[currentLevel] == gamePattern[currentLevel]) {
+// created function that check user input
+function checkAnswer(currentLevel) {
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
     console.log("success");
+    
+    //If the user got the most recent answer right check that they finish the sequence.
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function() {
+        nextSequence();
+      },1000);
+
+    } 
   } else {
     console.log("wrong");
   }
+  
 }
 
 function nextSequence() {
+
+ userClickedPattern = [];
 
   let randomNumber = Math.floor(Math.random() * 4);
   let randomChosenColour = buttonColours[randomNumber];
@@ -44,22 +49,24 @@ function nextSequence() {
   // flash button and play sound
   $(`#${randomChosenColour}`).fadeIn(100).fadeOut(100).fadeIn(100);
   playSound(randomChosenColour);
-  
-  // Check Which button is pressed
-
-  $(".btn").click(function() {
-      let userChosenColor = this.id;
-      userClickedPattern.push(userChosenColor);
-    
-      playSound(userChosenColor);
-      checkAnswer(level - 1);
-  });
 }
 
+// check if a key is pressed
 $(document).keydown(function(){
   if (!started) {
     $("#level-title").text(`Level ${level}`);
     nextSequence();
     started = true;
   }
+});
+
+  // Check Which button is pressed
+$(".btn").click(function() {
+  let userChosenColor = $(this).attr("id");
+  userClickedPattern.push(userChosenColor);
+
+  playSound(userChosenColor);
+  // animatePress(userChosenColor);
+  // Call checkAnswer()
+  checkAnswer(userClickedPattern.length - 1);
 });
